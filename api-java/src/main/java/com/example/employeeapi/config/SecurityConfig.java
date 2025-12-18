3c8194e2fdb423229558649b706332eadd99ead5
song-yx <song-yx@amitech.co.jp>
2025-12-18 18:58:21 +0900

    test

--- api-java/src/main/java/com/example/employeeapi/config/SecurityConfig.java
+++ api-java/src/main/java/com/example/employeeapi/config/SecurityConfig.java
@@ -2,17 +2,23 @@ package com.example.employeeapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
       .csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/api/**").permitAll()
+        .anyRequest().authenticated()
+      );
+
     return http.build();
   }
 }
