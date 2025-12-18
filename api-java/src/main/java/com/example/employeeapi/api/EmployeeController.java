9ab9f69959bcbe5d57df5b2f71dd9aecf5126655
song-yx <song-yx@amitech.co.jp>
2025-12-18 10:38:53 +0900

    TEST

--- api-java/src/main/java/com/example/employeeapi/api/EmployeeController.java
+++ api-java/src/main/java/com/example/employeeapi/api/EmployeeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/api/employees")
-@RequestMapping("/employees")
+@RequestMapping("/api/employees")
+//@RequestMapping("/employees")
 public class EmployeeController {
 
   private final EmployeeRepository repository;
